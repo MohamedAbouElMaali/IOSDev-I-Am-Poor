ebd0fb0b4eaf39db632034670413fb87d1a35166
Mohamed Abou El Maali <maboue85@gmail.com>
2023-08-09 15:15:08 -0400

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Mohamed Abouelmaali on 12/17/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
